f1d57d165e5a9ab39230bd8d68f93fc7b3231573
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-16 21:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-//complete this code
 class Animal {
-	constructor(species){
-		this._species = species
-	}
-get species(){
-	return this._species
+    constructor(species) {
+        this._species = species;
+    }
+    get species() {
+        return this._species;
+    }
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
 }
-makeSound(){
-	console.log(`The ${this.species} make a sound`)
-}
-}
-
 
 class Dog extends Animal {
-	 constructor(species){
-	  super(species)
-  }
-bark(){
-	console.log("woof")
-}
+    constructor(species) {
+        super(species);
+    }
+    bark() {
+        console.log("woof");
+    }
 }
 
 class Cat extends Animal {
-  constructor(species){
-	  super(species)
-  }
-purr(){
-	console.log("purr")
-}
-	
+    constructor(species) {
+        super(species);
+    }
+    purr() {
+        console.log("purr");
+    }
 }
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
